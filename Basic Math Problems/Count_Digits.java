d1d21832d089d5f05542f15987dd796560069036
Varun Yadav <onlinevarunyadav@gmail.com>
2026-08-11 19:38:49 +0530

    Add Count_Digits class to count the number of digits in a given number

---
+++ Basic Math Problems/Count_Digits.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+
+public class Count_Digits {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a number : ");
+        int num = sc.nextInt();
+        int orgNUm = num;
+        int ctr = 0;
+        while(num>0){
+            int lastDigit = num%10;
+            ctr++;
+            num = num/10;
+        }
+        System.out.println("Number of digits in "+orgNUm+" are : "+ctr+".");
+    }
+}
