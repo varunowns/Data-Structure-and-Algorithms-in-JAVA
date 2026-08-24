032854b3461cd0274f808d7c7eae4319971d9d07
Varun Yadav <onlinevarunyadav@gmail.com>
2026-08-24 20:29:54 +0530

    Refactor binary search methods in Prob_704 class for clarity and consistency

--- Binary Search/Prob_704.java
+++ Binary Search/Prob_704.java
@@ -1,7 +1,8 @@
 import java.util.Scanner;
 
+
 public class Prob_704 {
-    static int search(int nums[], int target){
+    static int search1(int nums[], int target){   //Brute Force Appproach
 
         int low = 0, high = nums.length-1, mid;
 
@@ -22,6 +23,22 @@ public class Prob_704 {
         return -1;
     }
 
+    static int search2(int nums[], int low, int high, int target){  //Recursive Approach
+        if (low>high) {
+            return -1;
+        }
+        int mid = (low+high)/2;
+
+        if (nums[mid] == target) {
+            return mid;
+        } else if (target > nums[mid]) {
+            return search2(nums, mid+1, high, target);
+        } else{
+            return search2(nums, low, mid-1, target);
+
+        }
+    }
+
     public static void main(String[] args) {
         int nums[] = {3,4,6,7,9,12,16,17}; 
 
@@ -30,8 +47,12 @@ public class Prob_704 {
         System.out.print("Enter target : ");
         int target = sc.nextInt();
 
-        int result = search(nums, target);
-        System.out.println("Index: " + result);
+        int result = search1(nums, target);
+        System.out.println("Index (result1): " + result);
+
+        int result2 = search2(nums, 0, nums.length, target);
+        System.out.println("Index (result2): " + result2);
+
 
         sc.close();
     }
