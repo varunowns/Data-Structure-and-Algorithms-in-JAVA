9cd9dd4d94c43333e9000a602c878c8b6a083f65
Varun Kumar Yadav <onlinevarunyadav@gmail.com>
2026-08-26 10:54:49 +0530

    Time: 0 ms (100%), Space: 45 MB (47.73%) - LeetHub

---
+++ LeetCode/0081-search-in-rotated-sorted-array-ii/0081-search-in-rotated-sorted-array-ii.java
@@ -0,0 +1,27 @@
+class Solution {
+    public boolean search(int[] nums, int target) {
+        int low = 0, high = nums.length - 1, mid;
+        while(low<=high){
+            mid = (low + high)/2;
+            if(nums[mid]==target) return true;
+            if(nums[low] == nums[mid] && nums[mid] == nums[high]){
+                low++; high--;
+            } else {
+                if(nums[low]<=nums[mid]){
+                    if(nums[low]<=target && nums[mid]>=target){
+                        high = mid - 1;
+                    } else {
+                        low = mid + 1;
+                    }
+                } else {
+                    if(nums[mid]<=target && nums[high]>=target){
+                        low = mid + 1;
+                    } else {
+                        high = mid - 1;
+                    }
+                }
+            }
+        }
+        return false;
+    }
+}
