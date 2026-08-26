c41af10bd74d31909095bda3fdde7e73a39b5662
Varun Yadav <onlinevarunyadav@gmail.com>
2026-08-26 09:29:41 +0530

    Implement SearchRotatedArray class to perform binary search on a rotated sorted array

---
+++ Binary Search/SearchRotatedArray.java
@@ -0,0 +1,46 @@
+import java.util.Scanner;
+
+public class SearchRotatedArray {
+    static int search(int nums[], int target){
+
+        int low = 0, high = nums.length, mid;
+
+        while (low<=high) {
+            mid = (low + high)/2;
+
+            if (nums[mid] == target) {
+                return mid;
+            }
+
+            if (nums[low] <= nums[mid]) { // left is sorted
+                if(nums[low] <= target && nums[mid] >=target){
+                    high =  mid - 1;
+                } else{
+                    low = mid + 1;
+                }
+            } else { // right is sorted
+                if(nums[mid] <= target && nums[high] >= target){
+                    low = mid + 1;
+                } else {
+                    high = mid - 1;
+                }
+            } 
+        }
+
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter element : ");
+        int target = sc.nextInt();
+        
+        int[] nums = {7,8,9,1,2,3,4,5,6};
+
+        int result = search(nums, target);
+
+        System.out.println("Index : " + result);
+
+        sc.close();
+    }
+}
