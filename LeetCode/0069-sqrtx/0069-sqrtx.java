04f53899bb8d6d1d79cd2b2331289c6827c3ef81
Varun Kumar Yadav <onlinevarunyadav@gmail.com>
2026-08-30 23:37:35 +0530

    Time: 1 ms (98.68%), Space: 42.6 MB (42.88%) - LeetHub

---
+++ LeetCode/0069-sqrtx/0069-sqrtx.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int mySqrt(int x) {
+        int low = 0;
+        int high = x;
+        int ans = 0;
+
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+
+            long square = (long) mid * mid;
+
+            if (square == x) {
+                return mid;
+            } 
+            else if (square > x) {
+                high = mid - 1;
+            } 
+            else {
+                ans = mid;
+                low = mid + 1;
+            }
+        }
+
+        return ans;
+    }
+}
