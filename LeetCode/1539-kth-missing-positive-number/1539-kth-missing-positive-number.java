65834d3af26cd8de6dea04228443dd387bf2b2e4
Varun Kumar Yadav <onlinevarunyadav@gmail.com>
2026-09-04 17:23:08 +0530

    Time: 0 ms (100%), Space: 44.4 MB (74.09%) - LeetHub

---
+++ LeetCode/1539-kth-missing-positive-number/1539-kth-missing-positive-number.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int findKthPositive(int[] arr, int k) {
+        int low = 0, high = arr.length - 1, mid, missingCtr = 0;
+        while(low<=high){
+            mid = (low + high)/2;
+            missingCtr = arr[mid] - (mid+1);
+            if(missingCtr < k){
+              low = mid + 1;
+            } else {
+                high = mid - 1;
+            }
+        }
+        return low + k;
+    }
+}
