1096faf75272c5e897de99ab0354c81f17c62716
Varun Yadav <onlinevarunyadav@gmail.com>
2026-09-26 03:03:49 +0530

    Implement searchMatrix method in Searcch2d_Matrix_II class for 2D matrix search

---
+++ Binary Search/Searcch2d_Matrix_II.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+public class Searcch2d_Matrix_II {
+    public static boolean searchMatrix(int[][] matrix, int target) {
+        int n = matrix.length;
+        int m = matrix[0].length;
+
+        int row = 0, col = m - 1;
+        while(row < n && col >= 0){
+            if(matrix[row][col] == target){
+                return true;
+            } else if(matrix[row][col] < target){
+                row++;
+            } else {
+                col--;
+            }
+        }
+        return false;
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int[][] matrix = {
+            {1, 4, 7, 11, 15},
+            {2, 5, 8, 12, 19},
+            {3, 6, 9, 16, 22},
+            {10, 13, 14, 17, 24},
+            {18, 21, 23, 26, 30}
+        };
+        System.out.print("Enter you target : ");
+        int target = sc.nextInt();
+
+        System.out.println(searchMatrix(matrix, target));
+        sc.close();
+    }
+}
